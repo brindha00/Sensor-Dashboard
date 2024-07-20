25f513af2e79c6f603469cc625ef4abcd1926936
brindha00 <brindhaselvan1231@gmail.com>
2024-07-20 11:22:51 +0530

    pdf

--- src/Report.js
+++ src/Report.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import * as XLSX from 'xlsx';
 import { saveAs } from 'file-saver';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 import './Report.css';
 import image from './img/Report.png';
 
 const Report = () => {
-  const handleDownload = () => {
-    // Define static data
-    const data = [
-      ["Temperature", "Sensor"],
-      [78, 1],
-      [67, 2],
-      [88, 3],
-    ];
+  const data = [
+    ["Temperature", "Sensor"],
+    [78, 1],
+    [67, 2],
+    [88, 3],
+  ];
 
+  const handleDownloadExcel = () => {
     // Create a worksheet
     const ws = XLSX.utils.aoa_to_sheet(data);
 
@@ -34,6 +35,32 @@ const Report = () => {
     return buf;
   };
 
+  const handleDownloadPDF = () => {
+    const doc = new jsPDF();
+
+    // Add the image on the first page
+    const imgData = image;
+    doc.addImage(imgData, 'PNG', 20, 20, 150, 150); // Adjust the coordinates and size as needed
+
+    // Add a new page for the content (table)
+    doc.addPage();
+
+    // Add a title for the content page
+    doc.text('Sensor Data', 20, 10);
+
+  
+
+    // Add a table on the second page
+    doc.autoTable({
+      head: [data[0]],
+      body: data.slice(1),
+      startY: 20,
+    });
+
+    // Save the PDF
+    doc.save('sensor-data.pdf');
+  };
+
   return (
     <div className='top1'>
       <div className='topin'>
@@ -42,8 +69,9 @@ const Report = () => {
       <div className='low1'>
         <div className='image'>
           <img src={image} alt="Report" style={{ width: '200px', height: '250px' }} />
-          <button className='but' onClick={handleDownload}>Download</button>
-          <h3 className='textt'>Click below to download the Excel file</h3>
+          <button className='but' onClick={handleDownloadExcel}>Download Excel</button>
+          <button className='but1' onClick={handleDownloadPDF}>Download PDF</button>
+          <h3 className='textt'>Click below to download the Excel or PDF file</h3>
         </div>
       </div>
     </div>
